7e6fd089d4e7e8fb44c1644e24b21274e77971f5
obama <baral.oh16@gmail.com>
2016-06-16 10:19:36 +0300

    stage commit

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,7 @@ public class Main {
      * The method that going to run when the program is executed.
      */
     public Main(){
-        String ip = "127.0.0.1";//JOptionPane.showInputDialog(null, "enter ip");
+        String ip = "192.168.1.110";//JOptionPane.showInputDialog(null, "enter ip");
         if(ip.equals("")){
             ip = "localhost";
         }
